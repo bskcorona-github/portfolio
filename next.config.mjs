58b546541ed3717385726ab22becef2941f04ade
bskcorona-github <kanemasa.tatsuro@gmail.com>
2025-06-04 14:53:20 +0900

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   images: {
     domains: ["images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
+  async redirects() {
+    return [];
+  },
+  async rewrites() {
+    return [];
+  },
+  trailingSlash: false,
+  swcMinify: true,
 };
 
 export default nextConfig;
